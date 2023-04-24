9d9636ae02540f3227fc059a3efcf536e5830a5a
zzoo <zzoolink@gmail.com>
2023-04-25 01:46:05 +0900

    plz

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	if err != nil {
 		log.Fatal("cannot load config:", err)
 	}
+	
 	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
